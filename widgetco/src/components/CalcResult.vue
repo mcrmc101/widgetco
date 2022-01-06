0fff7f9e50b7129009e2bf396fd890066d8008e0
mcrmc101 <mark@mcrmc.co.uk>
2022-01-06 13:09:24 +0000

    calc result

--- widgetco/src/components/CalcResult.vue
+++ widgetco/src/components/CalcResult.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <h1>{{ result }}</h1>
+    <b-list-group>
+      <b-list-group-item>Packs to Send:</b-list-group-item>
+      <b-list-group-item
+        class="d-flex justify-content-between"
+        v-for="(res,index) in this.$store.state.result"
+        :key="index"
+      >{{ res }} x {{ index }}</b-list-group-item>
+
+    </b-list-group>
   </div>
 </template>
 <script>
 export default {
   name: 'CalcResult',
-  data () {
-    return {
-      result: this.$store.state.result
-    }
-  }
 }
 </script>
